1c7744e8678039cd3c7cc4272290295530352f66
CarloEz <marcarlos47@gmail.com>
2019-11-04 14:44:29 -0600

    bd_nube

--- database.js
+++ database.js
@@ -1,12 +1,18 @@
 const mongoose=require('mongoose');
-//const URI='mongodb+srv://admin:admin@cluster0-rvkzp.mongodb.net/reserva?retryWrites=true&w=majority';
 
-const URI='mongodb://localhost/reserva';
+//conexion a la nube.
+const URI='mongodb+srv://admin:QxAJg6DqLgDUCiTE@cluster0-rvkzp.mongodb.net/reserva?retryWrites=true&w=majority';
+//const URI='mongodb://localhost/reserva';
+
+
+//opciones:
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
 
 mongoose.connect(URI)
 .then(db=>console.log('Conexion establecida'))
 .catch(err=>console.error(err));
-
-mongoose.set('useFindAndModify', false);
 
 module.exports=mongoose;
